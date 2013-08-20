8d46358f0f1c7af16a69bf18d6af9a363ad3df7e
Al <al@dallasmatthews.co.uk>
2013-08-21 00:08:16 +0200

    First commit

---
+++ application/logs/log-2013-08-21.php
@@ -0,0 +1,58 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2013-08-21 00:05:25 --> Config Class Initialized
+DEBUG - 2013-08-21 00:05:25 --> Hooks Class Initialized
+DEBUG - 2013-08-21 00:05:25 --> Utf8 Class Initialized
+DEBUG - 2013-08-21 00:05:25 --> UTF-8 Support Enabled
+DEBUG - 2013-08-21 00:05:25 --> URI Class Initialized
+DEBUG - 2013-08-21 00:05:25 --> Router Class Initialized
+DEBUG - 2013-08-21 00:05:25 --> No URI present. Default controller set.
+DEBUG - 2013-08-21 00:05:25 --> Output Class Initialized
+DEBUG - 2013-08-21 00:05:25 --> Security Class Initialized
+DEBUG - 2013-08-21 00:05:25 --> Input Class Initialized
+DEBUG - 2013-08-21 00:05:25 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-08-21 00:05:25 --> Language Class Initialized
+DEBUG - 2013-08-21 00:05:25 --> Loader Class Initialized
+DEBUG - 2013-08-21 00:05:25 --> Controller Class Initialized
+DEBUG - 2013-08-21 00:05:25 --> Model Class Initialized
+DEBUG - 2013-08-21 00:05:25 --> Model Class Initialized
+ERROR - 2013-08-21 00:05:25 --> Severity: Notice  --> Undefined property: Welcome::$db /Applications/MAMP/htdocs/projects/campaigndashboard.co.uk/system/core/Model.php 51
+DEBUG - 2013-08-21 00:05:33 --> Config Class Initialized
+DEBUG - 2013-08-21 00:05:33 --> Hooks Class Initialized
+DEBUG - 2013-08-21 00:05:33 --> Utf8 Class Initialized
+DEBUG - 2013-08-21 00:05:33 --> UTF-8 Support Enabled
+DEBUG - 2013-08-21 00:05:33 --> URI Class Initialized
+DEBUG - 2013-08-21 00:05:33 --> Router Class Initialized
+DEBUG - 2013-08-21 00:05:33 --> No URI present. Default controller set.
+DEBUG - 2013-08-21 00:05:33 --> Output Class Initialized
+DEBUG - 2013-08-21 00:05:33 --> Security Class Initialized
+DEBUG - 2013-08-21 00:05:33 --> Input Class Initialized
+DEBUG - 2013-08-21 00:05:33 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-08-21 00:05:33 --> Language Class Initialized
+DEBUG - 2013-08-21 00:05:33 --> Loader Class Initialized
+DEBUG - 2013-08-21 00:05:33 --> Controller Class Initialized
+DEBUG - 2013-08-21 00:05:33 --> Model Class Initialized
+DEBUG - 2013-08-21 00:05:33 --> Model Class Initialized
+ERROR - 2013-08-21 00:05:33 --> Severity: Notice  --> Undefined property: Welcome::$db /Applications/MAMP/htdocs/projects/campaigndashboard.co.uk/system/core/Model.php 51
+DEBUG - 2013-08-21 00:06:52 --> Config Class Initialized
+DEBUG - 2013-08-21 00:06:52 --> Hooks Class Initialized
+DEBUG - 2013-08-21 00:06:52 --> Utf8 Class Initialized
+DEBUG - 2013-08-21 00:06:52 --> UTF-8 Support Enabled
+DEBUG - 2013-08-21 00:06:52 --> URI Class Initialized
+DEBUG - 2013-08-21 00:06:52 --> Router Class Initialized
+DEBUG - 2013-08-21 00:06:52 --> No URI present. Default controller set.
+DEBUG - 2013-08-21 00:06:52 --> Output Class Initialized
+DEBUG - 2013-08-21 00:06:52 --> Security Class Initialized
+DEBUG - 2013-08-21 00:06:52 --> Input Class Initialized
+DEBUG - 2013-08-21 00:06:52 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-08-21 00:06:52 --> Language Class Initialized
+DEBUG - 2013-08-21 00:06:52 --> Loader Class Initialized
+DEBUG - 2013-08-21 00:06:52 --> Database Driver Class Initialized
+DEBUG - 2013-08-21 00:06:52 --> Controller Class Initialized
+DEBUG - 2013-08-21 00:06:52 --> Model Class Initialized
+DEBUG - 2013-08-21 00:06:52 --> Model Class Initialized
+DEBUG - 2013-08-21 00:06:52 --> File loaded: ../application/views/welcome_message.php
+DEBUG - 2013-08-21 00:06:52 --> Language file loaded: language/english/profiler_lang.php
+DEBUG - 2013-08-21 00:06:52 --> Helper loaded: text_helper
+DEBUG - 2013-08-21 00:06:52 --> Final output sent to browser
+DEBUG - 2013-08-21 00:06:52 --> Total execution time: 0.0639
