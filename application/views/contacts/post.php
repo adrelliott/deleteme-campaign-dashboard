35aa36a2aec5a0aa596fe24ada1e7c0ecb2fbafa
Al <al@dallasmatthews.co.uk>
2013-08-25 14:06:40 +0200

    Integrated datatables into Bootstrap

--- application/views/contacts/post.php
+++ application/views/contacts/post.php
@@ -1,29 +1,22 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-	<head>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		
-		<title>DataTables example</title>
+
 		<link href="<?php echo site_url('assets/css/demo_table.css'); ?>" rel="stylesheet">
 		<script type="text/javascript" charset="utf-8">
 			$(document).ready(function() {
 				$('#example').dataTable( {
 					"bProcessing": true,
+					"sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
 					"bServerSide": true,
 					"sAjaxSource": "<?php echo site_url('contacts/get_by_ajax'); ?>",
 					"sServerMethod": "POST"
 				} );
+				$.extend( $.fn.dataTableExt.oStdClasses, {
+    "sWrapper": "dataTables_wrapper form-inline"
+} );
 			} );
 		</script>
-	</head>
-	<body id="dt_example">
-		<div id="container">
-			<div class="full_width big">
-				DataTables server-side processing with POST example
-			</div>
-			
-			<div id="dynamic">
-<table cellpadding="0" cellspacing="0" border="0" class="display" id="example">
+	
+<table cellpadding="0" cellspacing="0" border="0" class="table table-striped table-bordered" id="example">
+
 	<thead>
 		<tr>
 			<th width="20%">Id</th>
@@ -32,14 +25,6 @@
 			<th width="25%">Owner Id</th>
 		</tr>
 	</thead>
-	<tbody>
-		<tr>
-			<td colspan="5" class="dataTables_empty">Loading data from server</td>
-		</tr>
-	</tbody>
+	
 	
 </table>
-			</div>
-			
-	</body>
-</html>
--- application/views/partials/bootstrap/_footer.php
+++ application/views/partials/bootstrap/_footer.php
@@ -5,17 +5,6 @@
 
 </div> <!-- /container -->
 
-<?php if (ENVIRONMENT === 'development'): ?> 
-	<script src="<?php echo base_url('assets/js/jquery-203.js'); ?>"></script>
-	<script src="<?php echo base_url('assets/js/jquery.dataTables.js'); ?>"></script>
-	<script src="<?php //echo base_url('assets/js/jquery-ui.min.js'); ?>"></script>
-	<script src="<?php //echo base_url('assets/lodash/lodash.min.js'); ?>"></script>
-<?php else : ?>
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-	<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
-	<script //src="http://cdnjs.cloudflare.com/ajax/libs/lodash.js/1.2.1/lodash.min.js"></script>
-<?php endif; ?>
-	<script src="<?php echo base_url('assets/js/bootstrap.js') ?>"></script>
 
 </body>
 </html>
