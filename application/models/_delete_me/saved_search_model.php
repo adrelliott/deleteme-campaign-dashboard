1b2ab5d0686ebe6615739aeba16affb705c429c1
Al <al@dallasmatthews.co.uk>
2013-10-16 21:50:09 +0200

    removed all extraneous files from project

---
+++ application/models/_delete_me/saved_search_model.php
@@ -0,0 +1,61 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Saved_search_model extends MY_Model {
+
+	//define what columns to return in a search
+	protected $_cols = array(
+         'single_record' => array(
+                          'id', 'search_name', 'search_description', 'search_type', 'query', 'owner_id'),
+         'multiple_record' => array(
+                          'id', 'search_name', 'search_description', 'search_type', 'query', 'owner_id')
+    );
+	
+	/*
+		You can set observers to call methods before create, update, get and delete
+		See here: https://github.com/jamierumbelow/codeigniter-base-model
+	 */
+
+	public function __construct() 
+	{ 
+        parent::__construct();
+    }
+
+    /**
+     * Gets a stat as a query, and returns the result of the query 
+     * @param  int $id Id of the search
+     * @return mixed     Can be an integer/float
+     */
+    public function get_stat($id)
+    {
+    	//get the stat id
+    	$stat = $this->get($id);
+
+    	//Run that query
+    	if ($stat->query)
+		{
+			$stat = $stat->query;
+			$result = $this->db->query($stat)->row();
+			return reset($result);	//Just returns the valueof $result[0]
+		}
+    }
+
+    /**
+     * Calculates change between two stats
+     * @param  int $id1 The id of search 1
+     * @param  int $id2 The id of search 2
+     * @param  string $type Type fo change, e.g. percentage
+     * @return number      can be a percentage, an integer or a float
+     */
+    public function get_change($id1, $id2, $type = 'pecentage')
+    {
+    	//do the two searches
+    	//
+    	//then calculate the change
+    	//
+    	//then return as percentage/integer/float as defined in $type
+    }
+
+}
+
+/* End of file xxxxxx.php */
+/* Location: ./application/controllers/welcome.php */
---
+++ application/views/partials/_delete_me/bootstrap-2/_header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta name="description" content="">
+   <meta name="keywords" content="">
+   <meta name="author" content="">
+
+   <title>Campaign Dashboard</title>
+
+   <link href="<?php echo base_url('assets/css/bootstrap.css') ?>" rel="stylesheet">
+   <link href="<?php echo base_url('assets/css/navbar-fixed-top.css') ?>" rel="stylesheet">
+   <link href="<?php //echo base_url('assets/css/bootstrap-responsive.min.css') ?>" rel="stylesheet">
+   <link href="<?php echo base_url('assets/css/datatables_table.css') ?>" rel="stylesheet">
+   <link href="<?php //echo base_url('assets/css/font-awesome.css') ?>" rel="stylesheet">
+   <link href="<?php echo base_url('assets/css/custom.css') ?>" rel="stylesheet">
+
+   <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="<?php echo base_url('assets/css/js/html5shiv.js'); ?>"></script>
+      <script src="<?php echo base_url('assets/css/js/respond.min.js'); ?>"></script>
+    <![endif]-->
+
+</head>
+<body>
