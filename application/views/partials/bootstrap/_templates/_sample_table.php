a4c046b08c11f83f4e664b327b9980536d82672c
Al Elliott <al@dallasmatthews.co.uk>
2013-10-22 21:03:45 +0200

    Responsive - Part 1: Added first templates

---
+++ application/views/partials/bootstrap/_templates/_sample_table.php
@@ -0,0 +1,54 @@
+<table class="table">
+	<thead>
+		<tr>
+			<th>Id</th>
+			<th>Task Name</th>
+			<th>Date</th>
+			<th></th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>1</td>
+			<td>xxxx</td>
+			<td>1/12/01</td>
+			<td>Del | Edit</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>xxxx</td>
+			<td>1/12/01</td>
+			<td>Del | Edit</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>xxxx</td>
+			<td>1/12/01</td>
+			<td>Del | Edit</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>xxxx</td>
+			<td>1/12/01</td>
+			<td>Del | Edit</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>xxxx</td>
+			<td>1/12/01</td>
+			<td>Del | Edit</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>xxxx</td>
+			<td>1/12/01</td>
+			<td>Del | Edit</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>xxxx</td>
+			<td>1/12/01</td>
+			<td>Del | Edit</td>
+		</tr>
+	</tbody>
+</table>	
