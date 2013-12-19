76088c1a85e191fe2fa4237301a09aa19207210b
Al Elliott <al@dallasmatthews.co.uk>
2013-12-19 15:09:20 +0000

    Incorporated Tellenis changes from 15.12

--- application/views/partials/bootstrap/_search/_row_basic.php
+++ application/views/partials/bootstrap/_search/_row_basic.php
@@ -35,9 +35,9 @@
             </li>
             <li class="list-group-item hide">
                 <div class="row">
-                    <div class="form-group col-lg-1 col-md-1 col-sm-2 col-xs-12">
+                    <div class="form-group col-lg-2 col-md-2 col-sm-2 col-xs-12">
                         <label class="sr-only" for="operator[]">Search type...</label>
-                        <?= form_dropdown('operator[]', array('or' => 'or', 'and' => 'and', 'not' => 'but not'), 'or', 'class="form-control input-sm"'); ?>
+                        <?= form_dropdown('operator[]', array('or' => 'Can have...', 'and' => 'Must have...', 'not' => 'Cannot have...'), 'or', 'class="form-control input-sm"'); ?>
                     </div>
 
                     <div class="form-group col-lg-3 col-md-3 col-sm-5 col-xs-12">
@@ -50,9 +50,9 @@
                         <?= form_dropdown('season[]', config('seasons', 'dropdowns'), '', 'class="form-control input-sm"'); ?>
                     </div>
 
-                    <div class="form-group col-lg-3 col-md-3 offset-md-2 col-sm-6 col-xs-12">
+                    <div class="form-group col-lg-2 col-md-2 offset-md-3 col-sm-6 col-xs-12">
                         <label class="sr-only" for="date_type[]"></label>
-                        <?= form_dropdown('date_type[]', array('' => 'I don\'t care when they ordered', 'before' => 'Ordered before', 'on' => 'Ordered on', 'after' => 'Ordered After'), '', 'class="form-control input-sm"'); ?>
+                        <?= form_dropdown('date_type[]', array('' => 'Ordered anytime', 'before' => 'Ordered before', 'on' => 'Ordered on', 'after' => 'Ordered After'), '', 'class="form-control input-sm"'); ?>
                     </div>
 
                     <div class="form-group col-lg-2 col-md-2 col-sm-6 col-xs-12">
